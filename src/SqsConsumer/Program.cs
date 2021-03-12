fd8b103192cad42169389ef1edd82b1c583d55d6
Hendry Anwar <hendryaw@amazon.com>
2021-03-12 13:33:01 +0700

    Initial commit

---
+++ src/SqsConsumer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SqsConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing Amazon SQS Client and getting queue URL...");
+            var sqsClient = new AmazonSQSClient();
+            var getQueueUrlRequest = new GetQueueUrlRequest
+            {
+                QueueName = "DemoMasterWorkerPattern",
+                QueueOwnerAWSAccountId = "545983628851"
+            };
+            var getQueueUrlResponse = sqsClient.GetQueueUrlAsync(getQueueUrlRequest).Result;
+            var queueUrl = getQueueUrlResponse.QueueUrl;
+            Console.WriteLine($"Received Queue URL: {queueUrl}");
+
+            var numberOfParallelTasks = 10;
+            Console.WriteLine($"Spawning {numberOfParallelTasks} to receive and process the messages");
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            // var task = Task.Run(() => {
+                Parallel.For(1, numberOfParallelTasks + 1, i => {
+                    Console.WriteLine($"Task {i}: Reading messages from queue.");
+                    while (true)
+                    {
+                        var receiveMessageRequest = new ReceiveMessageRequest
+                        {
+                            QueueUrl = queueUrl,
+                            MaxNumberOfMessages = 1,
+                            WaitTimeSeconds = 20
+                        };
+                        var receiveMessageResponse = sqsClient.ReceiveMessageAsync(receiveMessageRequest).Result;
+                        Console.WriteLine($"Task {i}: Received {receiveMessageResponse.Messages.Count} messages.");
+
+                        if (receiveMessageResponse.Messages.Count != 0)
+                        {
+                            var message = receiveMessageResponse.Messages[0];
+                            Console.WriteLine($"Task {i}: Processing messages...");
+                            Console.WriteLine($"Task {i}: Publisher said: '{message.Body}'");
+                            Thread.Sleep(2000);
+
+                            Console.WriteLine($"Task {i}: Deleting message from the queue...");
+                            sqsClient.DeleteMessageAsync(queueUrl, message.ReceiptHandle);
+                        }
+
+                        if (cancellationToken.IsCancellationRequested)
+                        {
+                            break;
+                        }
+                    }
+                });
+            // }, cancellationToken);
+
+            // Console.WriteLine("Running... Press any key to exit");
+            // Console.ReadLine();
+
+            // Console.WriteLine("Canceling gracefully...");
+            // cts.Cancel();
+            // while (!task.IsCompleted)
+            // {
+            //     Thread.Sleep(1000);
+            // }
+        }
+    }
+}
---
+++ src/SqsPublisher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SqsPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing Amazon SQS Client and getting queue URL...");
+            var sqsClient = new AmazonSQSClient();
+            var getQueueUrlRequest = new GetQueueUrlRequest
+            {
+                QueueName = "DemoMasterWorkerPattern",
+                QueueOwnerAWSAccountId = "545983628851"
+            };
+            var getQueueUrlResponse = sqsClient.GetQueueUrlAsync(getQueueUrlRequest).Result;
+            var queueUrl = getQueueUrlResponse.QueueUrl;
+            Console.WriteLine($"Received Queue URL: {queueUrl}");
+
+            while (true)
+            {
+                Console.Write("Type a message to be sent to the queue or type 'exit' to quit: ");
+                var message = Console.ReadLine();
+                if (string.Equals(message, "exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
+                Console.WriteLine($"Sending message with content '{message}' to the queue...");
+                var sendMessageRequest = new SendMessageRequest();
+                sendMessageRequest.QueueUrl = queueUrl;
+                sendMessageRequest.MessageBody = message;
+                var sendMessageResponse = sqsClient.SendMessageAsync(sendMessageRequest).Result;
+                Console.WriteLine("Message has been sent.");
+                Console.WriteLine($"HttpStatusCode: {sendMessageResponse.HttpStatusCode}");
+            }
+        }
+    }
+}
